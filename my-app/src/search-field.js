54868f28f6c40743f30f3302552d997639c7ae37
Yuliya Slavnova <yuliwna1@gmail.com>
2019-11-02 18:07:13 -0400

    updating search-field component

--- my-app/src/search-field.js
+++ my-app/src/search-field.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchDataThunk } from "./store/github-info-actions";
-import Container from "./Container";
 
 function groupBy(objArr, property) {
   return objArr.reduce((acc, obj) => {
@@ -14,113 +13,10 @@ function groupBy(objArr, property) {
   }, {});
 }
 
-function ShowData({ userNameOutput }) {
-  const [githubAPIData, getAPIData] = useState([]);
-  const [pullRequestData, getPullRequestData] = useState([]);
-  const [forkData, getForkData] = useState([]);
-  const [isLoading, getStatus] = useState(true);
-
-  useEffect(() => {
-    async function getData() {
-      try {
-        let data;
-        let response = await fetch(
-          `https://api.github.com/users/${userNameOutput}/events`
-        );
-        data = await response.json();
-
-        const filteredEventData = data.filter(item => {
-          return item.type === "PullRequestEvent" || item.type === "ForkEvent";
-        });
-        getAPIData(filteredEventData);
-      } catch (err) {
-        console.log("There is an error:", err.message);
-      }
-    }
-    getData();
-  }, [userNameOutput, getAPIData]);
-
-  useEffect(() => {
-    function filterData(data) {
-      const groupData = groupBy(data, "type");
-      console.log(groupData);
-
-      if (groupData.PullRequestEvent) {
-        getPullRequestData(groupData.PullRequestEvent);
-      }
-
-      if (groupData.ForkEvent) {
-        getForkData(groupData.ForkEvent);
-      }
-    }
-
-    function checkLoad(data) {
-      if (data.length === 0) {
-        getStatus(true);
-      }
-
-      if (data.length > 0) {
-        getStatus(false);
-      }
-    }
-
-    filterData(githubAPIData);
-    checkLoad(githubAPIData);
-  }, [githubAPIData]);
-
-  console.log("data", githubAPIData, forkData);
-
-  return (
-    <React.Fragment>
-      {Object.keys(githubAPIData).length === 0 && isLoading && (
-        <h1>Loading...</h1>
-      )}
-
-      {Object.keys(githubAPIData).length > 0 && (
-        <h1 className="text-right">{userNameOutput}</h1>
-      )}
-
-      {forkData.length > 0 && (
-        <Container data={forkData} title={"Recent Forks"} name={"fork"} />
-      )}
-      {pullRequestData.length > 0 && (
-        <Container
-          data={pullRequestData}
-          title={"Recent Pull Request"}
-          name={"pullRequest"}
-        />
-      )}
-    </React.Fragment>
-  );
-}
-
 function SearchField() {
-  const [githubUserName, getName] = useState("");
-  const [userNameOutput, passUserNameToAPI] = useState(githubUserName);
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    passUserNameToAPI(githubUserName);
-  };
-
   return (
     <React.Fragment>
-      {!userNameOutput && (
-        <form>
-          <label>
-            Github Username:
-            <input
-              type="text"
-              placeholder="Type something..."
-              value={githubUserName}
-              onChange={e => getName(e.target.value)}
-            ></input>
-          </label>
-          <input type="submit" onClick={handleSubmit}></input>
-        </form>
-      )}
-
-      {userNameOutput && <ShowData userNameOutput={userNameOutput} />}
+      <h1>test</h1>
     </React.Fragment>
   );
 }
